11d1242f4396a7deee3088712a401541198f560b
ektakumbhar210606-a11y <ektakumbhar210606@gmail.com>
2025-12-18 10:40:24 +0530

    Refactor Ternary.js to include additional console logs and a loop for better demonstration of functionality

--- Ternary.js
+++ Ternary.js
@@ -6,3 +6,13 @@ let grade = score >= 90 ? "A" :
             "Fail";
 
 console.log(grade); 
+
+let a=5;
+
+for(i=0;i<a;i++){
+    console.log(i);
+}
+
+let v='Having fun?';
+console.log(v.substring(7,10));
+
